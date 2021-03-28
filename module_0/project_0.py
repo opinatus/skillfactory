866e9c45fd112e88109edc15ab0769ca79fad364
Roman <this.harin@gmail.com>
2021-03-28 20:18:38 +0600

    'V1.1'

--- module_0/project_0.py
+++ module_0/project_0.py
@@ -8,33 +8,35 @@ Created on Sun Mar 28 12:27:47 2021
 import numpy as np
 
 def game_core_v3(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+    '''Угадать от 0 до 100. Начинаем с 50 и по условию или прибавляем, или отнимаем половину 50. Потом половину половины и так далее, 
+    пока число не стане  - more (или меньше - less) загаданного, параллельно сохраняя предпослений вариант. 
+    После этого находим между ними середину, пока не угадаем'''
     count = 1
     predict = 50
-    less = 1
-    more = 1
+    flag_less = 1
+    flag_more = 1
 
     while number != predict:
         count+=1
-
+        
+        # Если число больше:
         if number > predict:
-            less = 0
-            if more:
+            flag_less = 0
+            if flag_more:
                 previous_predict = predict
                 predict = predict + int((100-predict) / 2)
             else:
                 predict = int(np.mean([predict, previous_predict]))
-                less = 1
-
+                flag_less = 1
+        # Если число меньше:
         elif number < predict:
-            more = 0
-            if less:
+            flag_more = 0
+            if flag_less:
                 previous_predict = predict
                 predict = predict - int(predict/2)
             else:
                 predict = int(np.mean([predict, previous_predict]))
-                more = 1
+                flag_more = 1
 
     return(count) # выход из цикла, если угадали
    
@@ -42,7 +44,7 @@ def game_core_v3(number):
 def score_game(game_core):
 
     count_ls = [1]
-    #np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1,100, size=(1000))
     
     for number in random_array:
